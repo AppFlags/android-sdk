5b776767d61fd6fdd305bf858046e296d6d8a996
Justin Capogna <jcapogna@gmail.com>
2023-10-31 16:23:46 -0500

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppFlags Android SDK"
+include(":appflags-sdk-android")
